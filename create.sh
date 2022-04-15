e2ec41e77e8879deb87302bb8a655387b94a5a1e
Nicolas Fodor <nicolas@fodor.net>
2022-04-15 13:23:48 -0700

    Added create.sh as entry point.

--- create.sh
+++ create.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+export MONGO_ROOT_USER=admin
+export  MONGO_USER=adminman
+export  MONGO_DB=passpass
 
+docker-compose up --build --force-recreate --remove-orphans mongodb
--- init-mongo.sh
+++ init-mongo.sh
@@ -1,6 +1,6 @@
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
-    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var rootUser = '$MONGO_INITDB_ROOT_USER';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD_FILE';
     var admin = db.getSiblingDB('admin');
     admin.auth(rootUser, rootPassword);
 
